b7151dc23a74ebb55ea6dedbe90b97b1a91e5d16
Victor Parames <vparames2012@gmail.com>
2025-03-07 17:54:12 -0300

    Almost right code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  has_many :items, foreign_key: :users_id
+  has_many :orders, dependent: :destroy
 end
