74894de70b06bb6c914c87c6f8ddd2e91d17cad3
Lucas Silva <lucassfloriani@gmail.com>
2025-03-07 16:51:26 -0300

    seeds to be corrected

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :user
+  validates name, presence:true
+  validates price, presence:true
+  validates description,presence:true
+  validates categories,presence:true
+
 end
