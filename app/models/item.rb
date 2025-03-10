50f735685d1818b87d03f522f4af086406fdaed1
Pedro Toledo <pedrot1811@gmail.com>
2025-03-10 11:01:47 -0300

    added Order migrations and some model validations

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,6 @@ class Item < ApplicationRecord
   validates :name, presence: true
   validates :price, presence: true, numericality: { greater_than: 0 }
   validates :categories, inclusion: { in: %w(Tools SpaceShips Consumables) }
+  has_many :orders, dependent: :destroy
+  has_many :users, through: :orders
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   has_many :items, foreign_key: :users_id
   has_many :orders, dependent: :destroy
+  has_many :items, through: :orders
 end
