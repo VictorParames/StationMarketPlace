86d565bc2266e836ec86dcaaa08d243ddaf6f0c3
Lucas Silva <lucassfloriani@gmail.com>
2025-03-10 14:56:57 -0300

    again

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,42 @@
+class ItemsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @items = Item.all
+  end
+
+  def show
+    @item = Item.find(params[:id])
+    @user = current_user.id
+  end
+
+  def new
+    @item = current_user.items.build
+  end
+
+  def create
+    @item = current_user.items.build(item_params)
+    if @item.save
+      redirect_to items_path, notice: "Item successfully added to StationMarketPlace!"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+    @user = current_user.id
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to items_path, notice: "Item successfully patched"
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :price, :categories)
+  end
+end
